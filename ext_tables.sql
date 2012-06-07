6a21d5f0c0565b2a1ac98010b2043b4bb540e031
benjaminserfhos <benjaminserfhos@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-07 09:41:59 +0000

    * not used fields, removed..
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rk_metadata/trunk@63231 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,9 +2,6 @@
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
-	tx_rkmetadata_titletag varchar(255) DEFAULT '' NOT NULL
-	tx_rkmetadata_keywords_layout varchar(255) DEFAULT '' NOT NULL
-	tx_rkmetadata_descriptions_layout varchar(255) DEFAULT '' NOT NULL
 	tx_rkmetadata_seo int(11) DEFAULT '0' NOT NULL
 );
 
@@ -14,8 +11,5 @@ CREATE TABLE pages (
 # Table structure for table 'pages_language_overlay'
 #
 CREATE TABLE pages_language_overlay (
-	tx_rkmetadata_titletag varchar(255) DEFAULT '' NOT NULL
-	tx_rkmetadata_keywords_layout varchar(255) DEFAULT '' NOT NULL
-	tx_rkmetadata_descriptions_layout varchar(255) DEFAULT '' NOT NULL
 	tx_rkmetadata_seo int(11) DEFAULT '0' NOT NULL
 );

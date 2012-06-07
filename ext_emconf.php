675dd30641158b29aaabed2fd677851140185a24
benjaminserfhos <benjaminserfhos@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-07 09:12:08 +0000

    * updated to layouts typoscript support instead of page settings
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rk_metadata/trunk@63224 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rk_metadata".
 #
-# Auto generated 01-10-2011 19:30
+# Auto generated 07-06-2012 03:31
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,29 +11,38 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'RK Metadata',
+	'title' => 'Dynamic Metadata',
 	'description' => 'Generate keywords and title in different ways',
 	'category' => 'fe',
 	'shy' => 0,
 	'version' => '1.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
-	'priority' => '',
+	'priority' => 'bottom',
 	'loadOrder' => '',
-	'TYPO3_version' => '',
-	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'modify_tables' => 'pages',
+	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author' => 'Pim Broens',
 	'author_email' => 'extensions@redkiwi.nl',
-	'author_company' => '',
+	'author_company' => 'Redkiwi',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"100c";s:12:"ext_icon.gif";s:4:"90f6";s:17:"ext_localconf.php";s:4:"77cd";s:14:"ext_tables.php";s:4:"e4db";s:14:"ext_tables.sql";s:4:"6aa1";s:23:"locallang_csh_pages.php";s:4:"63f0";s:16:"locallang_db.xml";s:4:"62f6";s:14:"doc/manual.sxw";s:4:"471f";s:19:"static/ts/setup.txt";s:4:"38f5";s:33:"user/class.tx_rkmetadata_user.php";s:4:"91df";}',
+	'suggests' => array(
+	),
 );
 
 ?>

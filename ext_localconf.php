e02f4890a037a3eac019649c1d58ce7efd85e71f
benjaminserfhos <benjaminserfhos@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-18 14:46:51 +0000

    * removed conflict with old database
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rk_metadata/trunk@63559 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined ('TYPO3_MODE')) {
  	die ('Access denied.');
 }
 
-$TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_rkmetadata_titletag,tx_rkmetadata_keywords_layout,tx_rkmetadata_descriptions_layout,tx_rkmetadata_seo';
-$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_rkmetadata_titletag,tx_rkmetadata_keywords_layout,tx_rkmetadata_descriptions_layout,tx_rkmetadata_seo';
+$TYPO3_CONF_VARS['FE']['addRootLineFields'] .= ',tx_rkmetadata_seo';
+$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_rkmetadata_seo';
 
 ?>
